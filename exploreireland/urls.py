a1b3288b74129211ee30ca7451546a46312a5b20
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2022-07-19 12:04:07 +0000

    Add Authorization pages

--- exploreireland/urls.py
+++ exploreireland/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('locations.urls'), name='location_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
