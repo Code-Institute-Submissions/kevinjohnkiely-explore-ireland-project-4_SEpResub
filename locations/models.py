529c7284dc39ebdab86432f538a3c9af63e23500
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2022-07-19 08:03:54 +0000

    Add Location & Comment models

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Location(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="location_posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    location_image = CloudinaryField('image', default='placeholder_image')
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='location_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment_approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
